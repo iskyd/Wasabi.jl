d7761791cffec8df08ab4ce21db2241460e422c3
Documenter.jl <documenter@juliadocs.github.io>
2023-02-15 16:13:13 +0000

    build based on 9756867

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Wasabi","category":"page"},{"location":"#Wasabi","page":"Home","title":"Wasabi","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Wasabi.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Wasabi]","category":"page"}]
+}
