e73bd54e143fb162ace5c6d8173f3438d8492e2c
Mattia <mattia.careddu@conio.com>
2023-03-22 15:17:29 +0100

    remove unused code

--- test/runtests.jl
+++ test/runtests.jl
@@ -52,32 +52,3 @@ addtests("migrations.jl")
 addtests("postgres.jl")
 addtests("sqlite.jl")
 addtests("tablename.jl")
-
-# @testset ExtendedTestSet "Wasabi tests" begin
-#     mutable struct User <: Wasabi.Model
-#         id::Int
-#         name::String
-#     end
-
-#     struct UserProfile <: Wasabi.Model
-#         id::Int
-#         user_id::Int
-#         bio::Union{String,Nothing}
-#     end
-
-#     struct UserPhone <: Wasabi.Model
-#         id::Int
-#         user_profile_id::Int
-#         phone::String
-#     end
-
-#     Wasabi.primary_key(m::Type{User}) = Wasabi.PrimaryKeyConstraint(Symbol[:id])
-#     Wasabi.primary_key(m::Type{UserProfile}) = Wasabi.PrimaryKeyConstraint(Symbol[:id])
-
-#     Wasabi.foreign_keys(m::Type{UserProfile}) = [Wasabi.ForeignKeyConstraint(Symbol[:user_id], :user, Symbol[:id])]
-#     Wasabi.foreign_keys(m::Type{UserPhone}) = [Wasabi.ForeignKeyConstraint(Symbol[:user_profile_id], :user_profile, Symbol[:id])]
-
-#     Wasabi.unique_constraints(m::Type{UserProfile}) = [Wasabi.UniqueConstraint(Symbol[:user_id])]
-    
-#     @includetests ARGS
-# end
